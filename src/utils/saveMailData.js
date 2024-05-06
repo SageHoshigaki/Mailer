dc86e8810851d2695adbbc75cc56a9ec4af2f40a
Sage White <Sagewhite24@gmail.com>
2024-05-06 14:02:30 -0400

    api debug

--- src/utils/saveMailData.js
+++ src/utils/saveMailData.js
@@ -1,21 +1,36 @@
 import prisma from "@utils/prisma";
 
-// Script To Process Incoming Users Save And Sanitize Data
+// Script to process incoming mail data and save it to the database
 const saveMailData = async (req) => {
-  const { MailData } = req.body;
-  if (!MailData) {
-    throw new Error("MailData is missing from the request body");
+  const data = req.body;
+  if (!data) {
+    throw new Error("No data provided in the request body");
   }
+
+  const mailData = {
+    To_DebtCollectorName: data.contact_toReceiverName,
+    To_DebtCollectorAddress: data.contact_ToReceiverAddress,
+    To_DebtCollectorCity: data.contact_ToReceiverCity,
+    To_DebtCollectorState: data.contact_ToReceiverState,
+    To_DebtCollectorZipCode: data.contact_ToReceiverZipCode,
+    From_ContactFullName: data.contact_FromSenderName,
+    From_ContactAddress: data.contact_FromSenderAddress,
+    From_ContactCity: data.Contact_FromSenderCity,
+    From_ContactState: data.contact_FromSenderState,
+    From_ContactZipCode: data.contact_FromSenderZipCode,
+    document: data.MailData,
+    pdfLink: data.document_url,
+  };
 
   try {
     const savedData = await prisma.userMailService.create({
-      data: { ...MailData },
+      data: mailData,
     });
 
-    // Attach the saved data ID to the request object for the next middleware
+    // Optionally, you can attach the saved data ID to the request object for further processing
     req.savedData = {
-      document: savedData.document,
       id: savedData.id.toString(),
+      document: savedData.document,
     };
   } catch (error) {
     console.error("Error saving mail data:", error);
