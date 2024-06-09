8237a2f75c5874176dab99cc039b717f05a1ee07
Sage White <Sagewhite24@gmail.com>
2024-06-09 19:51:57 -0400

    open

--- src/utils/cloud/googlecloud.js
+++ src/utils/cloud/googlecloud.js
@@ -3,20 +3,28 @@ const { Storage } = require("@google-cloud/storage");
 const { loadGoogleCredentials } = require("@utils/googleCredentials");
 
 dotenv.config();
+
 async function uploadToGoogleCloud(filePath) {
+  console.log("Starting upload to Google Cloud...");
+
   // Load the credentials
-  await loadGoogleCredentials();
+  const credentialsPath = await loadGoogleCredentials();
+  console.log(`Google credentials loaded from: ${credentialsPath}`);
 
   // Initialize the Google Cloud Storage client
   const storage = new Storage();
   const bucketName = "gohigh-level-mail";
 
-  const fileName = filePath.split("/").pop();
+  const fileName = path.basename(filePath);
+  console.log(`Uploading file ${fileName} to bucket ${bucketName}`);
+
   await storage.bucket(bucketName).upload(filePath, {
     destination: fileName,
   });
 
-  return `https://storage.googleapis.com/${bucketName}/${fileName}`;
+  const fileUrl = `https://storage.googleapis.com/${bucketName}/${fileName}`;
+  console.log(`File uploaded to: ${fileUrl}`);
+  return fileUrl;
 }
 
 module.exports = uploadToGoogleCloud;
